e435aa9f45a6024c625294ca956f4ad3e3182690
malfoyz <the13mufasa@gmail.com>
2022-10-11 21:05:24 +0700

    'new_migrations'

---
+++ autoservice/main_app/models.py
@@ -0,0 +1,56 @@
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Section(models.Model):
+    """Модель раздела"""
+
+    title = models.CharField(
+        max_length=64,
+        verbose_name=_('Название'),
+    )
+    description = models.TextField(
+        verbose_name=_('Описание'),
+    )
+
+    class Meta:
+        verbose_name = _('Раздел')
+        verbose_name_plural = _('Разделы')
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Service(models.Model):
+    """Модель услуги"""
+
+    image = models.ImageField(
+        upload_to='services',
+        verbose_name=_('Изображение'),
+    )
+    title = models.CharField(
+        max_length=64,
+        verbose_name=_('Название'),
+    )
+    description = models.TextField(
+        verbose_name=_('Описание'),
+    )
+    price = models.DecimalField(
+        max_digits=13,
+        decimal_places=5,
+        verbose_name=_('Цена (от)'),
+    )
+    section = models.ForeignKey(
+        to=Section,
+        on_delete=models.CASCADE,
+        related_name='services',
+        related_query_name='service',
+    )
+
+    class Meta:
+        verbose_name = _('Раздел')
+        verbose_name_plural = _('Разделы')
+
+    def __str__(self) -> str:
+        return self.title
